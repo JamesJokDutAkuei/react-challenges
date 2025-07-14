724db00307d1691e80d37b14c000e25638167351
JamesJokDutAkuei <j.akuei@alutudent.com>
2025-07-14 16:34:02 +0200

    color hello world

---
+++ helloworld/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="square">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
